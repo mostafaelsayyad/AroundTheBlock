d4fdfe67dc27deeb884252fd712313601d2e0ce2
hagareldemairy <hagar.eldomairy@hotmail.com>
2016-06-29 06:57:40 +0200

    Go to place when click in nearby places

--- AroundTheBlock_Version2.2/app/src/main/java/com/example/hagarhossam/aroundtheblock_version2/Search/NearbySearch.java
+++ AroundTheBlock_Version2.2/app/src/main/java/com/example/hagarhossam/aroundtheblock_version2/Search/NearbySearch.java
@@ -1,15 +1,21 @@
 package com.example.hagarhossam.aroundtheblock_version2.Search;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.hagarhossam.aroundtheblock_version2.DatabaseManager.Database;
+import com.example.hagarhossam.aroundtheblock_version2.PlaceProfile.PlaceDetails;
 import com.example.hagarhossam.aroundtheblock_version2.R;
 
 import java.util.ArrayList;
@@ -23,12 +29,16 @@ public class NearbySearch extends ActionBarActivity {
     String longitude;
     ArrayList<ArrayList<String>> nearbyPlaces;
     String searchText="";
+    ListView _nearbyList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby_search);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        final String email = sharedPreferences.getString("email", "");
 
         ////////  Initialization  ////////
 
@@ -38,6 +48,8 @@ public class NearbySearch extends ActionBarActivity {
 
         Intent intent = getIntent();
         searchText = intent.getExtras().getString("searchText");
+
+        _nearbyList = (ListView)findViewById(R.id.nearbyPlaces);
 
         //////////////////////////////////
 
@@ -63,5 +75,35 @@ public class NearbySearch extends ActionBarActivity {
 
         ListView buckysListView = (ListView) findViewById(R.id.nearbyPlaces);
         buckysListView.setAdapter(buckysAdaptor);
+
+
+        ArrayList tempList = new ArrayList();
+        for (int i = 0; i < nearbyPlaces.size(); i++) {
+            tempList.add(nearbyPlaces.get(i).get(0));
+        }
+
+        //templist di arraylist feha 3 elements eli homa el names beto3 el places recommended
+
+        ArrayAdapter adapter2 = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, tempList);
+
+        _nearbyList.setAdapter(adapter2);
+
+        _nearbyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
+                String selectedFromList = (String) (_nearbyList.getItemAtPosition(myItemInt));
+
+                System.out.println("selected item in listview is " + selectedFromList);
+
+                ArrayList<String> placeDetails = new ArrayList<String>();
+                placeDetails = db.SelectPlaceDetailsGivenName(selectedFromList);
+
+                Intent intent = new Intent(NearbySearch.this, PlaceDetails.class);//mfrood mn place profile l place profile w 5alas kda 5eles el recommendation
+                intent.putStringArrayListExtra("placeDetails", placeDetails);
+                intent.putExtra("userId", email); // w hnak fil place profile page m7tag nest2blo b intent tnya
+
+                startActivity(intent);
+
+            }
+        });
     }
 }
